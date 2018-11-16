38b6e0580ceacb127541535ab968a96299645298
Ryan Efendy <ryanefendy95@gmail.com>
2018-11-15 23:56:30 -0800

    Remove app.js & update index.js :rocket::rocket::rocket:

--- src/App.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import MyReadApp from './components/MyReadApp';
-
-const App = () => <MyReadApp />;
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MyReadApp from './components/MyReadApp';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<MyReadApp />, document.getElementById('root'));
